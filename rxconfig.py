03128ab1e320244273ddb79f3a40e70a7702adb9
Tu Nombre <you@example.com>
2025-12-29 10:43:39 +0100

    Añadiendo mejoras y componentes

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Prueba_websocker2",
+    show_built_with_reflex=True,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
